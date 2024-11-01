98d8010f7987d26190b2111097fe64da7211fdc3
Rinor Rexhaj <105825061+RinorRexhaj@users.noreply.github.com>
2024-11-02 00:19:29 +0100

    Added keyword extraction

--- server/app.py
+++ server/app.py
@@ -1,8 +1,14 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from rake_nltk import Rake
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt_tab')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def home():
@@ -10,8 +16,15 @@ def home():
 
 @app.route('/', methods=['POST'])
 def receive_data():
-    data = request.json
-    return jsonify({"received_data": data})
+    data = request.json 
+    print(data)
+    rake_nltk_var = Rake()
+    if "prompt" in data:
+        rake_nltk_var.extract_keywords_from_text(data["prompt"])
+        keyword_extracted = rake_nltk_var.get_ranked_phrases()
+        return jsonify({"keywords": keyword_extracted})
+    else:
+        return jsonify({"error": "No 'text' field provided in the request data"}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
